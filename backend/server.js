81cf39c1c3e34c8ca08819db048fd06737420451
Maria Westling <maria_westling@hotmail.com>
2022-12-06 15:32:31 +0100

    register/log in works

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,10 @@
 import express from "express";
 import cors from "cors";
 import mongoose from "mongoose";
+import crypto from "crypto"
+import bcrypt from "bcrypt"
 
-const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/project-mongo";
+const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/project-auth";
 mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true });
 mongoose.Promise = Promise;
 
@@ -15,6 +17,128 @@ const app = express();
 // Add middlewares to enable cors and json body parsing
 app.use(cors());
 app.use(express.json());
+
+//
+const UserSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    unique: true,
+    required: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  accessToken: {
+    type: String,
+    default: () => crypto.randomBytes(128).toString("hex")
+  }
+})
+
+const User = mongoose.model("User", UserSchema)
+
+app.post("/register", async (req, res) => {
+  const { username, password } = req.body
+  // const code = [1, 2, 4, 4]
+  // const makeCodeSecret = (codeArr) => {
+  // const secretMessage = codeArr.map(singleNumber => singleNumber + 1)
+  // return secretMessage
+  // }
+  // transformedCode = makeCodeSecret(code)
+  try {
+    const salt = bcrypt.genSaltSync()
+    if (password.length < 8) {
+      res.status(400).json({
+        success: false,
+        response: "Password must be at least 8 characters long"
+      })
+    } else {
+      const newUser = await new User({ username: username, password: bcrypt.hashSync(password, salt) }).save()
+      res.status(201).json({
+        response: {
+          username: newUser.username,
+          accessToken: newUser.accessToken,
+          id: newUser._id
+        }
+      })
+    }
+  }
+  catch (error) {
+    res.status(400).json({
+      success: false,
+      response: error
+    })
+  }
+})
+
+app.post("/login", async (req, res) => {
+  const { username, password } = req.body
+
+  try {
+    const user = await User.findOne({ username })
+    if (user && bcrypt.compareSync(password, user.password)) {
+      res.status(200).json({
+        success: true,
+        response: {
+          username: user.username,
+          id: user._id,
+          accessToken: user.accessToken
+        }
+      })
+    } else {
+      res.status(400).json({
+        success: false,
+        response: "Credentials didn't match"
+      })
+    }
+  } catch (error) {
+    res.status(400).json({
+      success: false,
+      response: error
+    })
+  }
+})
+
+const authenticateUser = async (req, res, next) => {
+  const accessToken = req.header("Authorization")
+  try {
+    const user = await User.findOne({ accessToken: accessToken })
+    if (user) {
+      next()
+    } else {
+      res.status(401).json({
+        response: "Please log in",
+        success: false
+      })
+    }
+  } catch (error) {
+    res.status(400).json({
+      response: error,
+      success: false
+    })
+  }
+}
+
+const ThoughtSchema = new mongoose.Schema({
+  message: {
+    type: String,
+  },
+  createdAt: {
+    type: Date,
+    default: () => new Date()
+  },
+  hearts: {
+    type: Number,
+    default: 0
+  }
+})
+
+const Thought = mongoose.model("Thought", ThoughtSchema)
+app.get("/thoughts", authenticateUser)
+app.get("/thoughts", (req, res) => {
+  res.status(200).json({ success: true, response: "all the thoughts" })
+})
+
 
 // Start defining your routes here
 app.get("/", (req, res) => {
